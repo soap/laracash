c9af209c1d16a8ec6251efdf13d4a511a01de4ac
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2021-02-04 23:31:34 +0700

    Initial commit

---
+++ src/Facades/Laracash.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Soap\Laracash\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Laracash extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor(): string
+    {
+        return 'laracash';
+    }
+}
