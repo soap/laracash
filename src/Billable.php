737c318ea852d712dcc4c4f4aceaa78aecc2d38c
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2021-02-11 10:42:27 +0700

    Move to git

--- src/Billable.php
+++ src/Billable.php
@@ -2,8 +2,14 @@
 
 namespace Soap\Laracash;
 
+use NeptuneSoftware\Invoice\Traits\HasInvoice;
 
 trait Billable 
 {
-    
+    use HasInvoice; // enables the ->invoices() Eloquent relationship
+
+    public function invoiceFor()
+    {
+        
+    }
 }
