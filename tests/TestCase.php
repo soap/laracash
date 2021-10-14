fdfdf397070f0aeb1318be0016320260a85f952f
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2021-10-14 23:05:24 +0700

    Add test skeleton

---
+++ tests/TestCase.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Soap\Laracash\Tests;
+
+
+class TestCase extends \Orchestra\Testbench\TestCase
+{
+    /**
+     * Setup tests
+     * @return void 
+     */
+    public function setup(): void
+    {
+        Parent::setup();
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param \Illuminate\Foundation\Application $app
+     * @return void
+     */
+    protected function defineEnvironment($app)
+    {
+        // Setup default database to use sqlite :memory:
+        $app['config']->set('database.default', 'testbench');
+        $app['config']->set('database.connections.testbench', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ]);
+    }
+}
